3b2ef4d0ae11cda2fe22613423bda74447ed9cd2
Harry Hernández <harry.hernandez@upr.edu>
2016-11-10 23:07:49 -0400

    Modified login and notifications controllers.

--- app/controllers/NotificationsController.java
+++ app/controllers/NotificationsController.java
@@ -1,60 +1,35 @@
 package controllers;
 
-import play.mvc.*;
-import java.sql.*;
-import org.json.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import utilities.ToJSON;
+import utilities.MakeConnection;
+import models.Notifications;
 
 public class NotificationsController extends Controller {
 
     public Result getNotifications(Integer userID) {
-        System.out.println("Entro: " + userID);
+
         try {
-            String url = "jdbc:postgresql://localhost:5432/postgres";
-            Connection conn = DriverManager.getConnection(url,"abdielvega","abdiel123");
-            Statement stmt = conn.createStatement();
-            ResultSet rs;
-            rs = stmt.executeQuery("SELECT ntext FROM notifications WHERE  uid = " + userID);
-            String s = convertResultSetIntoJSON(rs).toString();
-            System.out.println(s);
-            conn.close();
-            
-            return ok(s);
+            MakeConnection db = new MakeConnection();
+            Connection activeConnection = db.connect();
+
+            ResultSet notificationText = Notifications.getNotificationsInfo(userID, activeConnection);
+
+            db.close();
+
+            String s = ToJSON.convertToJSONArray(notificationText).toString();
+
+            if(!s.equals("[]")) {
+                return ok(s);
+            }
+
         } catch (Exception e) {
             System.err.println("Got an exception! ");
             System.err.println(e.getMessage());
         }
-       
         return notFound("Failed!");
-
     }
-    
- 
-public static JSONArray convertResultSetIntoJSON(ResultSet resultSet) throws Exception {
-        JSONArray jsonArray = new JSONArray();
-        while (resultSet.next()) {
-            int total_rows = resultSet.getMetaData().getColumnCount();
-            JSONObject obj = new JSONObject();
-            for (int i = 0; i < total_rows; i++) {
-                String columnName = resultSet.getMetaData().getColumnLabel(i + 1).toLowerCase();
-                Object columnValue = resultSet.getObject(i + 1);
-                // if value in DB is null, then we set it to default value
-                if (columnValue == null){
-                    columnValue = "null";
-                }
-                /*
-                Next if block is a hack. In case when in db we have values like price and price1 there's a bug in jdbc - 
-                both this names are getting stored as price in ResulSet. Therefore when we store second column value,
-                we overwrite original value of price. To avoid that, i simply add 1 to be consistent with DB.
-                 */
-                if (obj.has(columnName)){
-                    columnName += "1";
-                }
-                obj.put(columnName, columnValue);
-            }
-            jsonArray.put(obj);
-        }
-        return jsonArray;
-    }   
-        
-
 }
