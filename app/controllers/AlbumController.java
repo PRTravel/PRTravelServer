c1ddaeb7364714614f68067790a11612c69903fe
Abdiel Vega <abdiel.vega2@upr.edu>
2016-11-12 01:39:17 -0400

    Album Backend

--- app/controllers/AlbumController.java
+++ app/controllers/AlbumController.java
@@ -1,12 +1,39 @@
 package controllers;
 
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import utilities.ToJSON;
+import utilities.MakeConnection;
+import models.Album;
 
 public class AlbumController extends Controller {
 
-    public Result getAlbums() {
+    public Result getAlbums(int userID) {
 
-        return ok("Success");
+         try {
+            MakeConnection db = new MakeConnection();
+            Connection activeConnection = db.connect();
+
+            ResultSet album = Album.getAlbumOfUser(userID, activeConnection);
+
+            db.close();
+
+            String s = ToJSON.convertToJSONArray(album).toString();
+            System.out.println("Hey");
+            System.out.println(s);
+            if(!s.equals("{}")){
+                return ok(s);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+       
+        return notFound("Failed!");
+
 
     }
 
---
+++ app/models/Album.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Album{
+
+    public static ResultSet getAlbumOfUser(int userID, Connection conn) throws SQLException{
+
+        PreparedStatement stmt;
+        ResultSet rs;
+
+        stmt = conn.prepareStatement("SELECT aid, albumname, alocation, aimageurl FROM albums natural inner join attractions natural inner join users WHERE uid = ?");
+        stmt.setInt(1, userID); 
+
+        rs = stmt.executeQuery();
+
+        return rs;
+    }
+}
