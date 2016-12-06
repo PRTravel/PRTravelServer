2d11992941e60f53dd0e36b3bd83e6b9659937f6
Abdiel Vega <abdiel.vega2@upr.edu>
2016-12-06 02:23:27 -0400

    Register

---
+++ app/controllers/SettingController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import utilities.ToJSON;
+import utilities.MakeConnection;
+import models.Setting;
+
+public class SettingController extends Controller {
+
+    public Result changePassword(Integer userID, String password) {
+        try {
+            MakeConnection db = new MakeConnection();
+            Connection activeConnection = db.connect();
+            
+            Setting.changePassword(userID, password, activeConnection);
+            
+            db.close();
+            return ok("Success");
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+        return notFound("Failed!");
+
+    }
+    
+        public Result changeEmail(Integer userID, String email) {
+        try {
+            MakeConnection db = new MakeConnection();
+            Connection activeConnection = db.connect();
+            
+            Setting.changeEmail(userID, email, activeConnection);
+            
+            db.close();
+            return ok("Success");
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+        return notFound("Failed!");
+
+    }
+
+}
