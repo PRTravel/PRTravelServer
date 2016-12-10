bc928f99006b6579103257fad99d5ac61a48d3b3
Harry Hernández <harry.hernandez@upr.edu>
2016-12-10 00:36:04 -0400

    Add albums now

--- app/controllers/AlbumController.java
+++ app/controllers/AlbumController.java
@@ -21,7 +21,7 @@ public class AlbumController extends Controller {
             db.close();
 
             String s = ToJSON.convertToJSONArray(album).toString();
-            if(!s.equals("{}")){
+            if(!s.equals("[]")){
                 return ok(s);
             }
 
@@ -33,6 +33,52 @@ public class AlbumController extends Controller {
         return notFound("Failed!");
 
 
+    }
+    
+    public Result getDiffAlbums(int userID) {
+
+         try {
+            MakeConnection db = new MakeConnection();
+            Connection activeConnection = db.connect();
+
+            ResultSet albums = Album.getDiffAlbums(userID, activeConnection);
+
+            db.close();
+
+            String s = ToJSON.convertToJSONArray(albums).toString();
+            if(!s.equals("[]")){
+                return ok(s);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+       
+        return notFound("Failed!");
+    }
+    
+    public Result addAlbum(int userID, int attractionID, String attractionName) {
+
+         try {
+            MakeConnection db = new MakeConnection();
+            Connection activeConnection = db.connect();
+
+            ResultSet albums = Album.addAlbum(userID, attractionID, attractionName, activeConnection);
+
+            db.close();
+
+            String s = ToJSON.convertToJSONArray(albums).toString();
+            if(!s.equals("[]")){
+                return ok(s);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+       
+        return notFound("Failed!");
     }
 
 }
--- app/models/Album.java
+++ app/models/Album.java
@@ -19,4 +19,31 @@ public class Album{
 
         return rs;
     }
+    
+    public static ResultSet getDiffAlbums(int userID, Connection conn) throws SQLException{
+
+        PreparedStatement stmt;
+        ResultSet rs;
+
+        stmt = conn.prepareStatement("SELECT aid, aimageurl, aname, alocation FROM attractions WHERE aid NOT IN (SELECT aid FROM albums WHERE uid = ?)");
+        stmt.setInt(1, userID); 
+
+        rs = stmt.executeQuery();
+
+        return rs;
+    }
+    
+    public static ResultSet addAlbum(int userID, int attractionID, String attractionName, Connection conn) throws SQLException{
+
+        PreparedStatement stmt;
+
+        stmt = conn.prepareStatement("INSERT INTO albums (uid, aid, albumname) VALUES (?, ?, ?)");
+        stmt.setInt(1, userID);
+        stmt.setInt(2, attractionID); 
+        stmt.setString(3, attractionName); 
+
+        stmt.executeUpdate();
+
+        return getAlbumOfUser(userID, conn);
+    }
 }
